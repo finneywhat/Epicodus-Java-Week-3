250a560f65768eacc9419104b04e3400cfa91ef2
Chris Finney <the.finney@gmail.com>
2017-03-31 09:45:52 -0700

    add getter methods for Client class and pass associated tests

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,9 +1,48 @@
 import org.sql2o.*;
 
 public class Client {
+  private int id;
+  private String name;
+  private String apptDate;
+  private String cutType;
+  private int stylistId;
 
-  public Client() {
-
+  public Client(String name, String apptDate, String cutType, int stylistId) {
+    this.name = name;
+    this.apptDate = apptDate;
+    this.cutType = cutType;
+    this.stylistId = stylistId;
   }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getApptDate() {
+    return apptDate;
+  }
+
+  public String getCut() {
+    return cutType;
+  }
+
+  public int getStylistId() {
+    return stylistId;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  @Override
+  public boolean equals(Object otherClient) {
+    if (!(otherClient instanceof Client)) {
+      return false;
+    } else {
+      Client newClient = (Client) otherClient;
+      return this.getName().equals(newClient.getName()) &&
+             this.getId() == newClient.getId();
+  }
+}
 
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -12,9 +12,48 @@ public class ClientTest {
   @After
   public void tearDown() {
     try (Connection con = DB.sql2o.open()) {
-      String sql = "DELETE FROM name_of_your_table *;";
+      String sql = "DELETE FROM clients *;";
       con.createQuery(sql).executeUpdate();
     }
   }
 
+  @Test
+  public void client_instantiatesCorrectly_true() {
+    Client myClient = new Client("Chris", "03-31-17", "low-fade", 1);
+    assertEquals(true, myClient instanceof Client);
+  }
+
+  @Test
+  public void getName_clientInstantiatesWithName_String() {
+    Client myClient = new Client("Chris", "03-31-17", "low-fade", 1);
+    assertEquals("Chris", myClient.getName());
+  }
+
+  @Test
+  public void getApptDate_clientInstantiatesWithApptDate_String() {
+    Client myClient = new Client("Chris", "03-31-17", "low-fade", 1);
+    assertEquals("03-31-17", myClient.getApptDate());
+  }
+
+  @Test
+  public void getCut_clientInstantiatesWithName_String() {
+    Client myClient = new Client("Chris", "03-31-17", "low-fade", 1);
+    assertEquals("low-fade", myClient.getCut());
+  }
+
+  @Test
+  public void getStylistId_clientInstantiatesWithName_int() {
+    Client myClient = new Client("Chris", "03-31-17", "low-fade", 1);
+    assertEquals(1, myClient.getStylistId());
+  }
+
+  @Test
+
 }
+
+// save()
+// all()
+// delete()
+// find()
+// update()
+// equals()
