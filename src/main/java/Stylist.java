d5d442b0164d139fda37a0b373ba3070524a345f
Chris Finney <the.finney@gmail.com>
2017-03-31 11:50:08 -0700

    add getter methods for instantiation, a getClients method and pass associated tests

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -1,9 +1,48 @@
 import org.sql2o.*;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Stylist {
+  private int id;
+  private String name;
+  private String hire_date;
+  private String base_salary;
+  private String work_schedule;
 
-  public Stylist() {
-
+  public Stylist(String name, String hire_date, String base_salary, String work_schedule) {
+    this.name = name;
+    this.hire_date = hire_date;
+    this.base_salary = base_salary;
+    this.work_schedule = work_schedule;
+    // employed = true;
   }
 
+  public String getName() {
+    return name;
+  }
+
+  public String getHireDate() {
+    return hire_date;
+  }
+
+  public String getSalary() {
+    return base_salary;
+  }
+
+  public String getSchedule() {
+    return work_schedule;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public List<Client> getClients() {
+  try(Connection con = DB.sql2o.open()) {
+    String sql = "SELECT * FROM clients WHERE stylist_id = :id";
+    return con.createQuery(sql)
+      .addParameter("id", this.id)
+      .executeAndFetch(Client.class);
+  }
+}
 }
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -17,4 +17,39 @@ public class StylistTest {
     }
   }
 
+  @Test
+  public void Stylist_instantiatesCorrectly_true() {
+    Stylist stylist = new Stylist("Dennis", "02-14-10", "30,000", "Mon, Tue, Thur, Sat");
+    assertTrue(stylist instanceof Stylist);
+  }
+
+  @Test
+  public void getName_stylistInstantiatesWithName_String() {
+    Stylist stylist = new Stylist("Dennis", "02-14-10", "30,000", "M-F");
+    assertEquals("Dennis", stylist.getName());
+  }
+
+  @Test
+  public void getHireDate_stylistInstantiatesWithHireDate_String() {
+    Stylist stylist = new Stylist("Dennis", "02-14-10", "30,000", "M-F");
+    assertEquals("02-14-10", stylist.getHireDate());
+  }
+
+  @Test
+  public void getSalary_stylistInstantiatesWithSalary_String() {
+    Stylist stylist = new Stylist("Dennis", "02-14-10", "30,000", "M-F");
+    assertEquals("30,000", stylist.getSalary());
+  }
+
+  @Test
+  public void getSchedule_stylistInstantiatesWithSchedule_String() {
+    Stylist stylist = new Stylist("Dennis", "02-14-10", "30,000", "M-F");
+    assertEquals("M-F", stylist.getSchedule());
+  }
+
+  @Test
+  public void getClients_initiallyReturnsEmptyList_ArrayList() {
+    Stylist stylist = new Stylist("Dennis", "02-14-10", "30,000", "M-F");
+    assertEquals(0, stylist.getClients().size());
+  }
 }
